e302a6949f3fdf23a2797d6543a7be2025774aba
minhphuong1994 <71524386+minhphuong1994@users.noreply.github.com>
2021-07-06 13:52:52 -0400

    moved user and password to .env file

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,8 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://postgres:mysecretpassword@localhost:5432/messenger", {
+const URI = `postgres://${process.env.USER}:${process.env.PASSWORD}@localhost:5432/messenger`
+
+const db = new Sequelize(process.env.DATABASE_URL || URI, {
   logging: false
 });
 
